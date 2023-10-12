1d6f44fad9ed2c98ae65cf1757356ad4ebcc4cca
Heokjin Yun <106298374+yhjin1096@users.noreply.github.com>
2023-10-12 11:02:41 +0900

    Delete build directory

--- build/viz_opencv/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/cona/3DVision-tutor/src/viz_opencv/include".split(';') if "/home/cona/3DVision-tutor/src/viz_opencv/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lviz_opencv".split(';') if "-lviz_opencv" != "" else []
-PROJECT_NAME = "viz_opencv"
-PROJECT_SPACE_DIR = "/home/cona/3DVision-tutor/devel"
-PROJECT_VERSION = "0.0.0"
