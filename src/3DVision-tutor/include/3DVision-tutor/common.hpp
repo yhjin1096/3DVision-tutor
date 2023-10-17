0d1b030629c21d25e3b36fba81427fcf15c05f7d
cona <yhjin1096@gmail.com>
2023-10-17 17:16:54 +0900

    Triangulation Done

--- src/3DVision-tutor/include/3DVision-tutor/common.hpp
+++ src/3DVision-tutor/include/3DVision-tutor/common.hpp
@@ -1,3 +1,4 @@
+/*------------------------------ ------------------------------*/
 #include <opencv2/opencv.hpp>
 #include <opencv2/features2d/features2d.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
@@ -12,13 +13,16 @@
 typedef Eigen::Matrix<double,4,4> Matrix4d;
 typedef Eigen::Matrix<double,4,3> Matrix4_3d;
 typedef Eigen::Matrix<double,3,3> Matrix3d;
+typedef Eigen::Matrix<float,3,3> Matrix3f;
 typedef Eigen::Matrix<double,2,2> Matrix2d;
+typedef Eigen::Matrix<float,2,2> Matrix2f;
 typedef Eigen::Matrix<double,2,1> Vector2d;
 typedef Eigen::Matrix<double,3,1> Vector3d;
 typedef Eigen::Matrix<double,4,1> Vector4d;
 typedef Eigen::Matrix<double,5,1> Vector5d;
 typedef Eigen::Matrix<double,6,1> Vector6d;
 typedef Eigen::Matrix<float,2,1> Vector2f;
+typedef Eigen::Matrix<float,3,1> Vector3f;
 typedef Eigen::Matrix<float,6,1> Vector6f;
 typedef Eigen::Matrix<float,72,1> Vector72f;
 typedef Eigen::Matrix<double,36,1> Vector36d;
