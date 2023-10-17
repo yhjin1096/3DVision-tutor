ed2854115308fcba01c6e76c79a6363fa9388d3e
Heokjin Yun <106298374+yhjin1096@users.noreply.github.com>
2023-10-17 18:05:39 +0900

    Delete build directory

--- build/3DVision-tutor/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "${prefix}/include".split(';') if "${prefix}/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-l3DVision-tutor".split(';') if "-l3DVision-tutor" != "" else []
-PROJECT_NAME = "3DVision-tutor"
-PROJECT_SPACE_DIR = "/home/cona/3DVision-tutor/install"
-PROJECT_VERSION = "0.0.0"
