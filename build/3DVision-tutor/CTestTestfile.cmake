f96155964013969f5c3331b22023492164639eb2
Heokjin Yun <106298374+yhjin1096@users.noreply.github.com>
2023-10-21 14:33:50 +0900

    Delete build directory

--- build/3DVision-tutor/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cona/3DVision-tutor/src/3DVision-tutor
-# Build directory: /home/cona/3DVision-tutor/build/3DVision-tutor
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/viz_pangolin/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cona/3DVision-tutor/src/viz_pangolin
-# Build directory: /home/cona/3DVision-tutor/build/viz_pangolin
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
